01d08159d125ad22c8e254fa0440b8a4e1cd851b
justinwoo <moomoowoo@gmail.com>
2018-05-10 15:23:21 +0300

    allow for specifying types of parameters in the template string

--- test/Main.purs
+++ test/Main.purs
@@ -5,15 +5,16 @@ import Prelude
 import Data.Either (Either(..))
 import Effect (Effect)
 import Effect.Console (log)
-import Kushiyaki (convertRecord, parseURL)
+import Kushiyaki (parseURL)
 import Test.Assert (assert)
 import Type.Prelude (SProxy(..))
 
 main :: Effect Unit
 main = do
-  let (parseURL'
-        -- inferred type:
-        :: String -> Either String { name :: String, age :: String })
+  -- let (parseURL'
+  --       -- inferred type:
+  --       :: String -> Either String { name :: String, age :: String })
+  let parseURL'
         = parseURL (SProxy :: SProxy "/hello/{name}/{age}")
   let parsed = parseURL' "/hello/Bill/12"
   case parsed of
@@ -24,12 +25,16 @@ main = do
       assert $ r.name == "Bill"
       assert $ r.age == "12"
 
-  let parseURL2' = parseURL (SProxy :: SProxy "/hello/{name}/{age}")
+  -- let (parseURL2'
+  --       -- inferred type:
+  --       :: String -> Either String { name :: String, age :: Int })
+  let parseURL2'
+        = parseURL (SProxy :: SProxy "/hello/{name:String}/{age:Int}")
   let parsed2 = parseURL2' "/hello/Bill/12"
-  case convertRecord =<< parsed of
+  case parsed2 of
     Left e -> do
       log $ "didn't work: " <> e
       assert $ 1 == 2
-    Right (r :: { name :: String, age :: Int }) -> do
+    Right r -> do
       assert $ r.name == "Bill"
       assert $ r.age == 12
